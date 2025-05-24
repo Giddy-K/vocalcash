8c3e1725af8ad0fbe8e6b63ef03057b60a0fb9fa
Giddy-K <gideonkipamet@gmail.com>
2025-05-25 01:17:21 +0300

    Added logout route

---
+++ app/logout/route.ts
@@ -0,0 +1,11 @@
+// app/logout/route.ts
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+
+export async function POST() {
+  const supabase = createRouteHandlerClient({ cookies })
+  await supabase.auth.signOut()
+  return NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'))
+}
