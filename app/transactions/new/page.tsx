fad8deba2a29ae311e1966f9e50620491f59862f
Giddy-K <gideonkipamet@gmail.com>
2025-05-25 02:08:49 +0300

    Minor changes

--- app/transactions/new/page.tsx
+++ app/transactions/new/page.tsx
@@ -1,10 +1,13 @@
-import TransactionForm from '@/components/TransactionForm'
+import TransactionForm from "@/components/TransactionForm";
 
 export default function NewTransactionPage() {
   return (
     <main className="max-w-xl mx-auto p-4">
+      <p className="italic text-gray-500">
+        📸 Photo receipt scanning coming soon…
+      </p>
       <h1 className="text-2xl font-bold mb-4">New Transaction</h1>
       <TransactionForm />
     </main>
-  )
+  );
 }
