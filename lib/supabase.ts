7106a2b8942148984e7ae4a343e340ccc0eafb7d
Giddy-K <gideonkipamet@gmail.com>
2025-05-25 01:18:57 +0300

    Minor changes on superbase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,4 @@
-import { createBrowserClient } from '@supabase/ssr'
+// lib/supabase.ts
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const supabase = createPagesBrowserClient()
