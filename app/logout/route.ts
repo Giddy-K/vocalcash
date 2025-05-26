a3551ffb218efb971fe354785a58579813278f6a
Giddy-K <gideonkipamet@gmail.com>
2025-05-26 14:10:14 +0300

    Modified this page

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,10 +1,9 @@
-// app/logout/route.ts
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies()
   const supabase = createRouteHandlerClient({
     cookies: () => cookieStore,
   });
